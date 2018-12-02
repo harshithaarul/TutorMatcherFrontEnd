023039afe1191dc56f78f68fba3b4f9ded0422df
Kailas Krivanka <38896831+KailasKrivanka@users.noreply.github.com>
2018-12-01 19:11:19 -0800

    final

--- API.js
+++ API.js
@@ -1,3 +1,4 @@
+var tutors = null;
 function login(user,pass) {
     var data = {
         todo:"login",
@@ -36,17 +37,20 @@ function findByKeyword() {
         keyword:keyword
     };
     console.log("sent");
+    clear();
+    if(keyword == null)return;
     axios.post('http://169.233.161.73:8080/', qs.stringify(data))
     .then(function (response) {
         if(response.data.message == "success"){
             var b = $('#main');
+            tutors = response.data.result;
             for (let i = 0; i < response.data.result.length; i++) {
                 var tutor = response.data.result[i];
                 console.log(tutor);
                 if(b.last().length >= 3){
                     b.append("<div class=\"w3-row-padding\"></div>");
                 }
-                // b.append(createNewElement());
+                b.last().append(createNewElement(tutor,i));
             }
         }
     })
@@ -56,8 +60,62 @@ function findByKeyword() {
     return false;
 }
 
-function createNewElement(){
+ function clear() {
+    var b = $('#main');
+    $('.w3-row-padding').remove();
+    $('.w3-container').remove();
+    b.append("<div class=\"w3-row-padding\"></div>");
+ }
 
+function createNewElement(tutor,index){
+    var n = "<div class=\"w3-third w3-container\" id=\""+index+"\">";
+    var subjects = "";
+    for (const subject of tutor.subjects) {
+        subjects += subject+", ";
+    }
+
+    n += "<div class=\"w3-container w3-white\">";
+    n += "<p><b>" + tutor.fName+ tutor.lName+"</b></p>";
+    n += addAttr("Teaches: ",subjects);
+    n += addAttr("Experience: ",tutor.exp);
+    n += addAttr("About: ",tutor.desc);
+    n += addAttr("Hourly Rate: ",tutor.pay);
+    n += addAttr("Availablility: ",tutor.available);
+    n += "<button onClick = \"matchTutor("+index+");\">Request</button><button onClick = \"remove("+index+");\">Decline</button>";
+    n += "</div>";
+    n += "</div>";
+    return n;
+}
+
+function addAttr(name,value) {
+    return "<p>"+name+value+"</p>";
+}
+
+function matchTutor(index){
+    if(tutors == null)return;
+    
+    var data = {
+        type:"tutee",
+        todo:"swipeRight",
+        tutee:sessionStorage.getItem("user").user,
+        tutor:tutors[index].user
+    };
+    console.log("sent");
+    axios.post('http://169.233.161.73:8080/', qs.stringify(data))
+    .then(function (response) {
+        console.log(response);
+        alert("Request sent!");
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+    return false;
+}
+
+function remove(index){
+    var x = "#"+index+"";
+    $(x).remove();
+    return false;
 }
 
 function getMatches() {
